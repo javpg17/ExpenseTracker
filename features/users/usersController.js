5f555fa34effcd78e8298a8c12e4fa096f750ff3
Javier Peña <japena@caracolknits.com>
2020-12-10 09:57:28 -0600

    Add the Register function and field validations

--- features/users/usersController.js
+++ features/users/usersController.js
@@ -1,9 +1,22 @@
 const { Router } = require('express');
 const express = require('express');
 const { check } = require('express-validator');
+const { fieldValidator } = require('../../middlewares/fieldValidator');
+const { CreateUser } = require('../users/userAppService');
 
 const router = express.Router();
 
-router.post();
+router.post(
+    '/register',
+    [
+        check('name', 'the name is required').not().isEmpty(),
+        check('email', 'the email is required').isEmail(),
+        check('password', 'the lenght must be greater than 4').isLength({
+            min: 4
+        }),
+        fieldValidator
+    ],
+    CreateUser
+);
 
 module.exports.router;
---
+++ middlewares/fieldValidator.js
@@ -0,0 +1,19 @@
+const { response } = require('express');
+const { validationResult } = require('express-validator');
+
+
+const fieldValidator = (req, res = response, next) => {
+    const fieldsValidations = validationResult(req);
+    if (!fieldsValidations.isEmpty()) {
+        const fieldsErrorMessage = fieldsValidations.errors[0].msg;
+        return res.status(400).json({
+            Message: fieldsErrorMessage,
+            Data: null
+        });
+    }
+    next();
+}
+
+module.exports = (
+    fieldValidator
+)
